a697c76b3f090065faa02d57b6a1e46ca07c3142
Drazka55 <61541118+Drazka55@users.noreply.github.com>
2021-05-02 22:00:33 +0200

    Added some object
    Added:
    Typing effect
    Simple description with animation
    down arrow
    Background circle

---
+++ app.js
@@ -0,0 +1,13 @@
+var i = 0;
+var txt = 'Hi, I am Pietro';
+var speed = 90;
+
+function typeWriter(){
+  if (i < txt.length){ /*ripeto questa funzione finche la variabile non e' uguale alla lunghezza dell'testo*/
+    document.getElementById('HiIamPietro').innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+typeWriter()
