8b0e9e091a243bc078f15b1091f288a3c2ac967b
Drazka55 <61541118+Drazka55@users.noreply.github.com>
2021-05-08 18:22:27 +0200

    Added the second project
    Modified some stuff on project section

--- app.js
+++ app.js
@@ -14,11 +14,16 @@ setTimeout(typeWriter, 375);
 
 function scrollAppear(){
   const project1 = document.querySelector('.projectOne');
-  const introPosition = project1.getBoundingClientRect().top; //Prendiamo la distanza dall' altro del nostro elemento
+  const introPosition1 = project1.getBoundingClientRect().top; //Prendiamo la distanza dall' altro del nostro elemento
+  const project2 = document.querySelector('.projectTwo');
+  const introPosition2 = project2.getBoundingClientRect().top;
   const screenPosition = window.innerHeight - 300; //Salviamo l'altezza della pagina in pixel
 
-  if(introPosition < screenPosition){
+  if(introPosition1 < screenPosition){
     project1.classList.add('projectOneVisible')//Aggiungiamo una classe
+  }
+  if(introPosition2 < screenPosition){
+    project2.classList.add('projectTwoVisible')//Aggiungiamo una classe
   }
 }
 
