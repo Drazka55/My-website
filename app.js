58fc4da90af715b47ec29e079bd041442580e8a9
Drazka55 <61541118+Drazka55@users.noreply.github.com>
2021-05-08 17:51:29 +0200

    Added appear effect
    Added appear effect and edited some stuff in css and javascript

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-var i = 0;
-var txt = 'Hi, I am Pietro';
-var speed = 90;
+let i = 0;
+const txt = 'Hi, I am Pietro';
+const speed = 90;
 
 function typeWriter(){
   if (i < txt.length){ /*ripeto questa funzione finche la variabile non e' uguale alla lunghezza dell'testo*/
@@ -11,3 +11,15 @@ function typeWriter(){
 }
 
 setTimeout(typeWriter, 375);
+
+function scrollAppear(){
+  const project1 = document.querySelector('.projectOne');
+  const introPosition = project1.getBoundingClientRect().top; //Prendiamo la distanza dall' altro del nostro elemento
+  const screenPosition = window.innerHeight - 300; //Salviamo l'altezza della pagina in pixel
+
+  if(introPosition < screenPosition){
+    project1.classList.add('projectOneVisible')//Aggiungiamo una classe
+  }
+}
+
+window.addEventListener('scroll', scrollAppear)
